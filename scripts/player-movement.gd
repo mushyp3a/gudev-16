5e44fc3f11a0cfb4334f7e29876205e53f2c78a8
mushypea <m.o.trafford@gmail.com>
2026-03-12 20:08:46 +0000

    character animation

--- scripts/player-movement.gd
+++ scripts/player-movement.gd
@@ -1,11 +1,17 @@
 extends CharacterBody2D
 
-const SPEED = 130.0
-const JUMP_VELOCITY = -300.0
-const DOUBLE_JUMP_VELOCITY = -270.0
-const WALL_JUMP_VELOCITY_X = 150.0
-const WALL_JUMP_VELOCITY_Y = -300.0
-const WALL_SLIDE_GRAVITY = 40.0
+const SPEED = 300.0
+const JUMP_VELOCITY = -800.0
+const DOUBLE_JUMP_VELOCITY = -800.0
+const WALL_JUMP_VELOCITY_X = 300.0
+const WALL_JUMP_VELOCITY_Y = -600.0
+const WALL_SLIDE_GRAVITY = 200.0
+const GRAVITY_MULT = 2
+const SLIDE_SPEED = 900.0
+const SLIDE_DURATION = 0.5
+const SLIDE_COOLDOWN = 0
+const SLIDE_JUMP_VELOCITY_Y = -800.0
+const SLIDE_JUMP_VELOCITY_X = 480.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -13,93 +19,146 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var floor = $FloorParticles
 @onready var wall = $WallParticles
 
+@onready var jumpFx = $JumpFx
+@onready var slideFx = $SlideFx
+
+@onready var animator = $Skeleton2D/hips/AnimationPlayer
+@onready var skeleton = $Skeleton2D
+
 var has_double_jump := true
 var is_wall_sliding := false
 var wall_jump_cooldown := 0.0
 var last_wall_normal := Vector2.ZERO
 
+var is_sliding := false
+var slide_timer := 0.0
+var slide_cooldown_timer := 0.0
+var slide_direction := 1.0
+
+func play_anim(anim_name: String):
+	if animator.current_animation != anim_name:
+		animator.play(anim_name, 0.15)
+
 func _ready():
 	ShaderManager.go_to_plan()
 
 func _physics_process(delta):
-
 	if _get_input_direction() != 0:
 		ShaderManager.go_to_run()
 
+	if slide_timer > 0:
+		slide_timer -= delta
+	if slide_cooldown_timer > 0:
+		slide_cooldown_timer -= delta
 	if wall_jump_cooldown > 0:
 		wall_jump_cooldown -= delta
+
+	if is_sliding:
+		if slide_timer <= 0:
+			is_sliding = false
+		else:
+			var slide_progress = 1.0 - (slide_timer / SLIDE_DURATION)
+			velocity.x = slide_direction * lerp(SLIDE_SPEED, SPEED, slide_progress)
 
 	is_wall_sliding = false
 	if not is_on_floor():
 		if is_on_wall() and _get_input_direction() != 0 and velocity.y > 0:
 			is_wall_sliding = true
 			wall.emitting = true
+			slideFx.play()
 			last_wall_normal = get_wall_normal()
 			velocity.y = move_toward(velocity.y, WALL_SLIDE_GRAVITY, gravity * delta)
 		else:
-			velocity.y += gravity * delta
+			velocity.y += gravity * delta * GRAVITY_MULT
 			wall.emitting = false
+			slideFx.stop()
 
 	if is_on_floor():
+		if is_sliding:
+			slideFx.play()
+		else:
+			slideFx.stop()
 		has_double_jump = true
 		wall_jump_cooldown = 0.0
 		wall.emitting = false
+		if not is_sliding and slide_cooldown_timer <= 0:
+			if Input.is_action_just_pressed("crouch"):
+				var dir = _get_input_direction()
+				slide_direction = dir if dir != 0 else (1.0 if skeleton.scale.x > 0 else -1.0)
+				is_sliding = true
+				slide_timer = SLIDE_DURATION
+				slide_cooldown_timer = SLIDE_COOLDOWN
+				ShaderManager.trigger_hit()
 
 	var direction = _get_input_direction()
 
 	if Input.is_action_just_pressed("restart"):
-			#get_tree().reload_current_scene()
 		ShaderManager.go_to_plan()
 
 	if Input.is_action_just_pressed("jump"):
-		if is_on_floor():
+		ShaderManager.go_to_run()
+		if is_sliding:
+			is_sliding = false
+			slide_timer = 0.0
+			velocity.y = SLIDE_JUMP_VELOCITY_Y
+			velocity.x = slide_direction * SLIDE_JUMP_VELOCITY_X
+			# jumpFx.play()
+			# floor.restart()
+			# floor.emitting = true
+			# ShaderManager.trigger_hit()
+		elif is_on_floor():
 			velocity.y = JUMP_VELOCITY
+			jumpFx.play()
 			floor.restart()
 			floor.emitting = true
-
 		elif is_wall_sliding:
 			if direction > 0 and wall.position.x < 0:
 				wall.position.x *= -1
 			elif direction < 0 and wall.position.x > 0:
 				wall.position.x *= -1
-
 			velocity.x = last_wall_normal.x * WALL_JUMP_VELOCITY_X
 			velocity.y = WALL_JUMP_VELOCITY_Y
-			has_double_jump = true # Reward the wall jump with a fresh double jump
+			has_double_jump = true
 			wall_jump_cooldown = 0.18
-
+			jumpFx.play()
 		elif has_double_jump:
 			ShaderManager.trigger_hit()
 			velocity.y = DOUBLE_JUMP_VELOCITY
 			has_double_jump = false
+			jumpFx.play()
 			floor.restart()
 			floor.emitting = true
 
+	if not is_sliding:
+		if direction > 0:
+			skeleton.scale.x = 1
+		elif direction < 0:
+			skeleton.scale.x = -1
 
-	if direction > 0:
-		animated_sprite.flip_h = false
-	elif direction < 0:
-		animated_sprite.flip_h = true
-
-	if wall_jump_cooldown > 0 and not is_on_floor():
+	if is_sliding:
+		pass
+	elif wall_jump_cooldown > 0 and not is_on_floor():
 		velocity.x = move_toward(velocity.x, direction * SPEED, SPEED * 3 * delta)
 	elif direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	# --- ANIMATIONS ---
-	# if is_wall_sliding:
-	# 	animated_sprite.play("wall_slide")
-	# elif is_on_floor():
-	# 	if direction == 0:
-	# 		animated_sprite.play("idle")
-	# 	else:
-	# 		animated_sprite.play("run")
-	# elif velocity.y < 0:
-	# 	animated_sprite.play("jump")
-	# elif not has_double_jump and velocity.y > 0:
-	# 	animated_sprite.play("fall")
+	if is_sliding:
+		play_anim("slide")
+	elif is_wall_sliding:
+		play_anim("wall_slide")
+	elif is_on_floor():
+		if abs(velocity.x) < 1:
+			play_anim("idle")
+		else:
+			play_anim("run")
+	elif velocity.y < 0 and has_double_jump:
+		play_anim("jump")
+	elif velocity.y < 0 and !has_double_jump:
+		play_anim("double_jump")
+	else:
+		play_anim("fall")
 
 	move_and_slide()
 
--- scripts/shader-manager.gd
+++ scripts/shader-manager.gd
@@ -35,75 +35,106 @@ const PHASE_RUN = {
 }
 
 var current_phase := "plan"
-var transition_tween : Tween
-
-var material : ShaderMaterial
-
 var is_transitioning := false
-
+var transition_tween : Tween
+var music_tween : Tween
+var material : ShaderMaterial
 var music : AudioStreamPlayer2D
 
 const PLAN_PITCH = 0.5
 const RUN_PITCH = 1.0
-var music_tween : Tween
 
 func _ready():
 	await get_tree().process_frame
+	_fetch_nodes()
+	get_tree().node_added.connect(_on_node_added)
+
+func _on_node_added(node: Node):
+	if node.name == "Game":
+		await get_tree().process_frame
+		_fetch_nodes()
+
+func _fetch_nodes():
 	material = get_tree().root.get_node("Game/cyberpunk-shader/ColorRect").material
 	music = get_tree().root.get_node("Game/AudioStreamPlayer2D")
-	music.pitch_scale = 0.5
+	music.pitch_scale = PLAN_PITCH
+	current_phase = "plan"
+	is_transitioning = false
 	apply_phase_instant(PHASE_PLAN)
 
 func go_to_run():
 	if current_phase == "run" or is_transitioning:
 		return
 	current_phase = "run"
-	_transition_to(PHASE_RUN, 0.6)
 	_tween_pitch(RUN_PITCH, 0.6)
+	_glitch_transition(PHASE_RUN, 0.5)
 
 func go_to_plan():
 	if current_phase == "plan" or is_transitioning:
 		return
 	current_phase = "plan"
-	_transition_to(PHASE_PLAN, 1.8)
-	_tween_pitch(PLAN_PITCH, 1.8)
+	_tween_pitch(PLAN_PITCH, 2.0)
+	_glitch_transition(PHASE_PLAN, 1.2)
 
-func _tween_pitch(target: float, duration: float):
-	print("tween pitch called, music: ", music, " target: ", target)
-	if !music:
-		print("music is null, aborting")
-		return
-	print("pitch before: ", music.pitch_scale)
-	if music_tween:
-		music_tween.kill()
-	music_tween = create_tween()
-	print("tween created")
-	music_tween.tween_property(music, "pitch_scale", target, duration)\
-		.set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
-	print("tween started")
-
-func _transition_to(phase: Dictionary, duration: float):
+func _glitch_transition(target_phase: Dictionary, settle_duration: float):
 	is_transitioning = true
 	if transition_tween:
 		transition_tween.kill()
 	transition_tween = create_tween()
-	transition_tween.set_parallel(true)
-	for key in phase:
-		var from = material.get_shader_parameter(key)
-		var to = phase[key]
-		transition_tween.tween_method(
-			func(v): material.set_shader_parameter(key, v),
-			from, to, duration
-		).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+	transition_tween.set_parallel(false)
 
-	await transition_tween.finished
-	is_transitioning = false
+	transition_tween.tween_callback(func():
+		material.set_shader_parameter("glitch_intensity", 1.0)
+		material.set_shader_parameter("chroma_strength", 9.0)
+		material.set_shader_parameter("saturation", 1.5)
+		material.set_shader_parameter("flicker_intensity", 0.2)
+	)
+	transition_tween.tween_interval(0.06)
+
+	transition_tween.tween_callback(func():
+		material.set_shader_parameter("glitch_intensity", 0.0)
+		material.set_shader_parameter("chroma_strength", 0.0)
+		material.set_shader_parameter("saturation", 0.1)
+		material.set_shader_parameter("flicker_intensity", 0.0)
+	)
+	transition_tween.tween_interval(0.05)
+
+	transition_tween.tween_callback(func():
+		material.set_shader_parameter("glitch_intensity", 0.85)
+		material.set_shader_parameter("chroma_strength", 7.0)
+		material.set_shader_parameter("saturation", 1.5)
+		material.set_shader_parameter("vignette_strength", 0.5)
+	)
+	transition_tween.tween_interval(0.07)
+
+	transition_tween.tween_callback(func():
+		var settle = create_tween()
+		settle.set_parallel(true)
+		for key in target_phase:
+			var from = material.get_shader_parameter(key)
+			var to = target_phase[key]
+			settle.tween_method(
+				func(v): material.set_shader_parameter(key, v),
+				from, to, settle_duration
+			).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+		settle.finished.connect(func(): is_transitioning = false, CONNECT_ONE_SHOT)
+	)
 
 func apply_phase_instant(phase: Dictionary):
 	for key in phase:
 		material.set_shader_parameter(key, phase[key])
 
+func _tween_pitch(target: float, duration: float):
+	if !music or !is_instance_valid(music):
+		return
+	if music_tween:
+		music_tween.kill()
+	music_tween = create_tween()
+	music_tween.tween_property(music, "pitch_scale", target, duration)\
+		.set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+
 func trigger_hit(intensity: float = 1.0):
+	is_transitioning = false
 	if transition_tween:
 		transition_tween.kill()
 
@@ -115,9 +146,9 @@ func trigger_hit(intensity: float = 1.0):
 	material.set_shader_parameter("saturation", 2.2)
 	material.set_shader_parameter("contrast", 1.6)
 
+	var recover_time = 0.35 + (0.2 * intensity)
 	var hit_tween = create_tween()
 	hit_tween.set_parallel(true)
-	var recover_time = 0.35 + (0.2 * intensity)
 
 	hit_tween.tween_method(func(v): material.set_shader_parameter("chroma_strength", v),
 		8.0 * intensity, base["chroma_strength"], recover_time).set_trans(Tween.TRANS_EXPO).set_ease(Tween.EASE_OUT)
