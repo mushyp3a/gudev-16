7ba440ae7e17cc71286c7b279f21b29f0ccb3e3b
Oliver Petchey <olliesp07@gmail.com>
2026-03-13 17:33:10 +0000

    Added some basic (unimplemented) scripts which will represent the different possible player actions

---
+++ scripts/player actions/player-interact-action.gd
@@ -0,0 +1,6 @@
+class_name PlayerInteractAction extends Action
+
+# Add logic here to make clone interact with levels or what have you
+
+func act():
+	pass
---
+++ scripts/player actions/player-sliding-action.gd
@@ -0,0 +1,6 @@
+class_name PlayerSlidingAction extends Action
+
+# Add logic here to make clone play sliding animation
+
+func act():
+	pass
